e4cf3affe7e4f3b118290bf61d372d2640577f6e
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-04-24 06:48:12 +0200

    add removeFromCart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -11,24 +11,36 @@ const cartSlice = createSlice({
             const newItem = action.payload;
             const existingCartItem = state.items.find(item => item.id === newItem.id)
 
+            state.totalQuantity++
+
             if (!existingCartItem) {
                 state.items.push({
-                    itemId: newItem.id,
+                    id: newItem.id,
                     price: newItem.price,
                     quantity: 1,
                     totalPrice: newItem.price,
                     name: newItem.title
                 })
             } else {
-                existingCartItem.quantity += newItem.quantity;
+                existingCartItem.quantity++;
                 existingCartItem.totalPrice += newItem.price
             }
 
         },
-        removeItemFromCart() { },
+        removeItemFromCart(state, action) {
+            const id = action.payload
+            const exisitingItem = state.items.find(item => item.id === id)
+            state.totalQuantity--
+
+            if (exisitingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id)
+            } else {
+                exisitingItem.quantity--;
+            }
+        },
 
     }
 })
 
-export const uiActions = cartSlice.actions;
+export const cartActions = cartSlice.actions;
 export default cartSlice
