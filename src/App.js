5c872f604015120bca13181c0b44a886ab394e2a
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-04-25 18:32:12 +0200

    Send cart to firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -6,7 +7,18 @@ import Products from './components/Shop/Products';
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisble);
-  console.log(showCart);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    console.log(cart);
+
+    fetch("https://authmaxmilum-default-rtdb.firebaseio.com/cart.json",
+      {
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      })
+  }, [cart])
+
   return (
     <Layout>
       {showCart && <Cart />}
